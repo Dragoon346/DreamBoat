ca0f6d7b99cd1670ff4a97dd2188f90d269223af
Dragoon346 <48118552+Dragoon346@users.noreply.github.com>
2019-04-04 08:39:34 -0400

    Add files via upload

--- GeneralPlayerScript.cs
+++ GeneralPlayerScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GeneralPlayerScript : MonoBehaviour
@@ -8,55 +9,87 @@ public class GeneralPlayerScript : MonoBehaviour
     public GameObject player; //reference to the player's gameobject(used to destroy it later)
     public float yrotation = 60.0f; // speed multiplier for y axis rotation
     public float xrotation = 60.0f; // speed multiplier for x axis rotation
-    public float forwards = 5f;// speed multiplier for moving forwards
-    public float backwards = -4f;// speed multiplier for moving backwards
-    public float strafe = 5f;// speed multiplier for moving	left or right
+    public float forwards = 7f;// speed multiplier for moving forwards
+    public float backwards = -6f;// speed multiplier for moving backwards
+    public float strafe = 7f;// speed multiplier for moving	left or right
     public bool isGrounded = false;//useless bool value that I can't get to function, not really needed, but it would be help to figure out any issues with movement/gravity
+    public bool running;
     public float jumpHeight = 0f;// base height for jumping, to be used in either a move function, or a lerped translate
     public float speed = 10f;//base speed multiplier for player movement
     public float gravity = 9.8f;//base speed for gravity
     private Vector3 move = Vector3.zero;//resets motion to zero
     public CapsuleCollider playercollider;// reference to a capsule collider on the player if I ever decide to add one for calculating forces and stuff
-	
-	void Update ()// Update is called once per frame
+    
+   void Update ()// Update is called once per frame
     {
-        float y = yrotation * -Input.GetAxis("Mouse Y");// sets the float y to use the same value as yrotation and then multiplies that by the movement of the y axis of the mouse
+        float y = -yrotation * Input.GetAxis("Mouse Y");// sets the float y to use the same value as yrotation and then multiplies that by the movement of the y axis of the mouse
         float x = xrotation * Input.GetAxis("Mouse X");// sets the float x to use the same value as xrotation and then multiplies that by the movement of the x axis of the mouse
         move = new Vector3(Input.GetAxis("Strafe"), 0, Input.GetAxis("Forwards")); //creates a new Vector3 for the values of movement
+
         move = transform.TransformDirection(move); //this basically isolates the movement to axes relative to the player instead of world axes
+
         move = move * speed; //multiplies the value of move by the multiplier for player's speed
+
         move.y = move.y + (jumpHeight - gravity);//moves the player down by gravity multiplied by Time.deltaTime
-        Camera.transform.Rotate(-y * Time.deltaTime, 0, 0);//rotates vertical rotation of the camera but not the character
+
+        Camera.transform.Rotate(- y * Time.deltaTime, 0, 0);//rotates vertical rotation of the camera but not the character
+
         playercon.transform.Rotate(0, x * Time.deltaTime, 0);// rotates the character and the camera horizontally, because the camera is a child of the character
-        playercon.Move(move * Time.deltaTime * speed);// Moves the player. Movement is equal to base move speed * Time.deltaTime * the speed multiplier
+        playercon.Move(move * Time.deltaTime * speed);// Moves the player. Movement is equal to base move speed * Time.deltaTime * the speed multiplier 
+        Cursor.lockState = CursorLockMode.Locked;//locks the cursor
+        Cursor.visible = false;//hides the cursor
+        if (Input.GetKey(KeyCode.LeftShift) && playercon.isGrounded == true)//if left shift is pressed and player is grounded
+        {
+            running = true;//running
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))//if you release the left shift key
+        {
+            running = false;//not running
+        }
+        if (running == false)//if not running
+        {
+            speed = 7f;//sets speed to 5
+        }
+        if (running == true)//if running
+        {
+            speed = 10f;//sets speed to 10
+        }
+       
         if (playercon.isGrounded == true)//if the player is resting on a surface with a collider
         {
             jumpHeight = 0f;// set   jump height to 0
             StopCoroutine("gravitySpeed");//stops adding speed to gravity when you touch the ground
-            gravity = 9.8f;// resets gravity to 9.8 u/s 
+            gravity = 9.8f;// resets gravity to 1500 u/s 
             if (Input.GetKey(KeyCode.Space))// if you press space while grounded
             {
                 Debug.Log("jump attempted");//console message telling me when I attempt to jump
-                StartCoroutine("jumping");//starts a coroutine for jumping
+                StartCoroutine("jumping");//starts the coroutine for jumping
             }
         }
         if (playercon.isGrounded == false)//if the player is not touching a surface with collision
         {
             StartCoroutine("gravitySpeed");//starts increasing gravity exponentially
         }
+        if (Input.GetKey("escape"))//if you press the escape key
+        {
+            Application.Quit();// quits the game
+        }
     }
+    
+
     private IEnumerator gravitySpeed()
     {
-        yield return new WaitForSecondsRealtime(0.0166f);//waits for 60th of a second realtime
-        gravity = gravity + 0.163f;// adds (1/60)gravity to gravity every 60th of a second for as long as the player is not grounded
+        yield return new WaitForSecondsRealtime(0.0167f);//waits for 60th of a second realtime
+        gravity = gravity + (0.016666f * gravity);// adds ((change in time since last frame update)+gravity) to gravity every 60th of a second for as long as the player is not grounded
     }
     private IEnumerator jumping()
     {
         gravity = 0f;//sets gravity to 0 at the start of the jump
-        jumpHeight = 10f;//sets jumpHeight to 10
-        yield return new WaitForSecondsRealtime(0.25f);//waits for a quarter of a second realtime
-        jumpHeight = 9.9f;//sets jumpHeight to 9.9
-        gravity = 9.8f; //sets gravity to 9.8
+        jumpHeight = 9f;//sets jumpHeight to 10
+        yield return new WaitForSecondsRealtime(0.3f);//waits for a quarter of a second realtime
+ 
+         jumpHeight = 13;//sets jumpHeight to 9.9
+        gravity = 9.8f; //sets gravity to 1 for a slower decline
     }
     void OnCollisionEnter(Collision collisionInfo)//Updates when you begin to touch something
     {
@@ -79,6 +112,7 @@ public class GeneralPlayerScript : MonoBehaviour
         if (collisionInfo.gameObject.tag == "extremoenemy")//if you enter the trigger area of the "extremoenemy"
         {
             Destroy(player, 0f);//you get destroyed immediately
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
         }
     }
 }
